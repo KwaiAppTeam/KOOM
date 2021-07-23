70caf93ea99cce49d426b0d2578a6c2f6ea30dd3
xueqiushi <xueqiushi@kuaishou.com>
2021-07-23 14:26:17 +0800

    Add kwai-unwind library
    Signed-off-by: xueqiushi <xueqiushi@kuaishou.com>

---
+++ kwai-unwind/src/main/cpp/unwind.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_kwai_unwind_NativeLib_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
