eef4cb18a0af09730d93c4755abf2dab025c6b83
陈海伦 <1217003634@qq.com>
2022-09-16 10:47:46 +0800

    隐私合规

---
+++ koom-monitor-base/src/main/java/com/kwai/koom/base/utils/ProcessUtil.java
@@ -0,0 +1,68 @@
+package com.kwai.koom.base.utils;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Build;
+import android.text.TextUtils;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.lang.reflect.Method;
+
+/**
+ * @Created by Hailun on 2022/8/23 15:52
+ * @Description 获取当前进程名工具类
+ */
+public class ProcessUtil {
+    private static String currentProcessName;
+    /**
+     * @return 当前进程名
+     */
+    @Nullable
+    public static String getCurrentProcessName(@NonNull Context context) {
+        if (!TextUtils.isEmpty(currentProcessName)) {
+            return currentProcessName;
+        }
+
+        //1)通过Application的API获取当前进程名
+        currentProcessName = getCurrentProcessNameByApplication();
+        if (!TextUtils.isEmpty(currentProcessName)) {
+            return currentProcessName;
+        }
+
+        //2)通过反射ActivityThread获取当前进程名
+        currentProcessName = getCurrentProcessNameByActivityThread();
+
+        return currentProcessName;
+    }
+
+    /**
+     * 通过Application新的API获取进程名，无需反射，无需IPC，效率最高。
+     */
+    public static String getCurrentProcessNameByApplication() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            return Application.getProcessName();
+        }
+        return null;
+    }
+
+    /**
+     * 通过反射ActivityThread获取进程名，避免了ipc
+     */
+    public static String getCurrentProcessNameByActivityThread() {
+        String processName = null;
+        try {
+            final Method declaredMethod = Class.forName("android.app.ActivityThread", false, Application.class.getClassLoader())
+                    .getDeclaredMethod("currentProcessName", (Class<?>[]) new Class[0]);
+            declaredMethod.setAccessible(true);
+            final Object invoke = declaredMethod.invoke(null, new Object[0]);
+            if (invoke instanceof String) {
+                processName = (String) invoke;
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return processName;
+    }
+}
